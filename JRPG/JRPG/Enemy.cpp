66b2f66c527b3b4013e6962a995704df9c2b402f
Baetaekyung <tkwayi2129@gmail.com>
2024-06-05 10:47:30 +0900

    스텟

--- JRPG/JRPG/Enemy.cpp
+++ JRPG/JRPG/Enemy.cpp
@@ -3,10 +3,50 @@
 
 Enemy::Enemy(int health, int damage, int agility, int armor, int criticalPercent, int criticalMultiplier)
 {
-	Stat::maxHealth = health;
+	stat.maxHealth = health;
+	stat.strength = damage;
+	stat.agility = agility;
+	stat.armor = armor;
+	stat.ciritalChance = criticalPercent;
+	stat.ciritalDamage = criticalMultiplier;
 }
 
 void Enemy::GetDamage(int damage)
 {
 	
+}
+
+void Enemy::Update()
+{
+
+}
+
+void Enemy::Render()
+{
+
+}
+
+void Enemy::Attack(IDamageable target)
+{
+
+}
+
+void Enemy::Defence(int damage)
+{
+
+}
+
+void Enemy::RunAway()
+{
+
+}
+
+void Enemy::UseItem()
+{
+
+}
+
+void Enemy::UseSkill()
+{
+
 }
--- JRPG/JRPG/Enemy.h
+++ JRPG/JRPG/Enemy.h
@@ -7,5 +7,16 @@ public:
 		int criticalPercent, int criticalMultiplier);
 public:
 	void GetDamage(int damage);
+public:
+	Stat stat;
+
+	// Object을(를) 통해 상속됨
+	void Update() override;
+	void Render() override;
+	void Attack(IDamageable target) override;
+	void Defence(int damage) override;
+	void RunAway() override;
+	void UseItem() override;
+	void UseSkill() override;
 };
 
